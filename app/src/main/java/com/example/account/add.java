f40d57ed3c5aa7db5f512ace69669db938b58c57
HW1 <jabez0324@gmail.com>
2019-04-24 22:16:03 +0800

    add fin

--- app/src/main/java/com/example/account/add.java
+++ app/src/main/java/com/example/account/add.java
@@ -1,7 +1,9 @@
 package com.example.account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class add extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class add extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+    public void fin(View v){
+        finish();
     }
 }
