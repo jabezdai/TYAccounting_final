d42ed7566b1bf1fc13ae3355878686e60647829b
D0543150 <yhc78887242@gmail.com>
2019-04-24 20:57:42 +0800

    Account

---
+++ app/src/main/java/com/example/account/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.account;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add(View v){
+        Intent it = new Intent(this,add.class);
+
+        startActivity(it);
+    }
+}
