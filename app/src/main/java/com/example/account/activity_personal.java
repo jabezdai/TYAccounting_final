21c7ac57bd3bcb722f97364d6df887d62971422e
D0543150 <yhc78887242@gmail.com>
2019-06-02 15:20:32 +0800

    個人資料頁面改成資料庫版存取(炸開的版本)

--- app/src/main/java/com/example/account/activity_personal.java
+++ app/src/main/java/com/example/account/activity_personal.java
@@ -1,7 +1,11 @@
 package com.example.account;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.renderscript.ScriptGroup;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,13 +17,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class activity_personal extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
 
-
-    String[] Memo={"現金","悠悠卡","點點卡","信用卡"};
+    static final String DB_NAME="dollarDB";
+    static final String TB_NAME="dollarlist";
+    static final int MAX = 10;
+    static final String[] FROM = new String[] {"name","money"};
+    SQLiteDatabase db;
+    Cursor cur;
+    SimpleCursorAdapter adapter;
+    EditText money;
     ListView lv;
-    ArrayAdapter<String> aa;
 
 
 
@@ -27,11 +37,40 @@ public class activity_personal extends AppCompatActivity implements AdapterView.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+        db = openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE,null);
 
+        String createTable = "CREAT TABLE IF NOT EXISTS" + TB_NAME
+                                +"(_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                +"(name VARCHAR(16),"
+                                +"money VARCHAR(16))";
+        db.execSQL(createTable);
+
+        cur = db.rawQuery("SELECT*FROM" + TB_NAME,null);
+
+        if(cur.getCount() == 0){
+            addDATA("現金",0);
+            addDATA("悠悠卡",0);
+            addDATA("點點卡",0);
+            addDATA("信用卡",0);
+        }
+
+        adapter = new SimpleCursorAdapter(this,R.layout.personal_list,cur,FROM, new int[]{R.id.name,R.id.money},0);
         lv = (ListView) findViewById(R.id.plv);
-        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Memo);
-        lv.setAdapter(aa);
+        lv.setAdapter(adapter);
         lv.setOnItemLongClickListener(this);
+    }
+    private void addDATA(String name,int money){
+        ContentValues cv = new ContentValues(2);
+        cv.put(FROM[0],name);
+        cv.put(FROM[1],money);
+
+        db.insert(TB_NAME,null,cv);
+    }
+    private void update(int money,int id){
+        ContentValues cv = new ContentValues(1);
+        cv.put(FROM[1],money);
+
+        db.update(TB_NAME,cv,"_id="+id,null);
     }
 
     public boolean onItemLongClick(AdapterView<?> a,View v,int pos,long id){
@@ -51,10 +90,8 @@ public class activity_personal extends AppCompatActivity implements AdapterView.
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
-
-                Memo[num] =Memo[num]+ "                                                                            "+in.getText().toString();
-                aa.notifyDataSetChanged();
+                int i = Integer.parseInt(in.getText().toString());
+                update(i,num);
 
             }
         });
